82fe27cd37bc54d4a6e96e701c25717a053e3ff3
unknown <D.Pevkin@DE.YURIDAN.BY>
2018-01-03 14:39:45 +0300

    woooot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import moose from './moose.png';
 import Progress from 'react-progressbar';
 import Upgrades from './Upgrades.js';
 
@@ -8,69 +7,12 @@ import Upgrades from './Upgrades.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    
-
-    this.mooseClickHandler = this.mooseClickHandler.bind(this);
-    this.upgradeClickHandler = this.upgradeClickHandler.bind(this);
   }
-
-  componentWillMount() {
-    if (localStorage.getItem("clicked") !== '') {
-      this.setState({ clicked: +localStorage.getItem("clicked"), clickCounter: +localStorage.getItem("clickCounter") });
-    }
-
-    window.addEventListener("beforeunload", (ev) => {
-      localStorage.setItem("clicked", this.state.clicked);
-      localStorage.setItem("clickCounter", this.state.clickCounter);
-    });
-  }
-
-  mooseClickHandler() {
-    this.setState({ clicked: this.state.clicked + this.state.clickCounter });
-  }
-
-  upgradeClickHandler(minMooseForUpgrade, incrClick) {
-    console.log(minMooseForUpgrade, incrClick);
-
-    if (this.state.clicked >= minMooseForUpgrade) {
-      this.setState({
-        clicked: this.state.clicked - minMooseForUpgrade,
-        clickCounter: this.state.clickCounter + incrClick
-      });
-      // localStorage.setItem('clickCounter', this.state.clickCounter, 'clicked', this.state.clicked - this.state.hornsMin);
-    }
-  }
-
-
+  
   render() {
     return (
       <div className="App">
-        <div className="Moose">
-          <img src={moose} onClick={this.mooseClickHandler} alt="moose" />
-        </div>
-        
           <Upgrades />
-          {/* <div className="upgrade" onClick={this.upgradeClickHandler.bind(this, this.state.hornsMin, 1)}>
-            <progress value={(this.state.clicked / this.state.hornsMin) * 100} max="100" />
-            Рога({this.state.hornsMin})
-          </div>
-          <div className="upgrade" onClick={this.upgradeClickHandler.bind(this, this.state.bloomMin, 2)}>
-            <progress value={(this.state.clicked / this.state.bloomMin) * 100} max="100" />
-            Шерсть({this.state.bloomMin})
-          </div>
-          <div className="upgrade" onClick={this.upgradeClickHandler.bind(this, this.state.sleighMin, 3)}>
-            <progress value={(this.state.clicked / this.state.sleighMin) * 100} max="100" />
-            Санки({this.state.sleighMin})
-          </div>
-          <div className="upgrade" onClick={this.upgradeClickHandler.bind(this, this.state.scooterMin, 4)}>
-            <progress value={(this.state.clicked / this.state.scooterMin) * 100} max="100" />
-            Скутер({this.state.scooterMin})
-          </div> */}
-        
-        <div className="scoreboard">
-          Лосиков: {this.state.clicked}
-        </div>
-
       </div>
     );
   }
--- src/Upgrades.js
+++ src/Upgrades.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Progress from 'react-progressbar';
+import moose from './moose.png';
 var upgrades = require('./upgradesConfig.json');
 var config = require('./config.json');
+
 
 export default class Upgrades extends Component {
     constructor(props) {
         super(props);
-
 
         this.state = {
             clicked: config.clicked,
@@ -18,39 +19,59 @@ export default class Upgrades extends Component {
             sleighMin: config.sleighMin,
             scooterMin: config.scooterMin
         }
+        this.mooseClickHandler = this.mooseClickHandler.bind(this);
+        this.upgradeClickHandler = this.upgradeClickHandler.bind(this);
 
-        // this.upgradeClickHandler = this.upgradeClickHandler.bind(this);
-        this.clicked1 = this.clicked1.bind(this);
     }
 
-    // upgradeClickHandler(minMooseForUpgrade, incrClick) {
-    //     console.log(minMooseForUpgrade, incrClick);
+    componentWillMount() {
+        if (localStorage.getItem("clicked") !== '') {
+            this.setState({ clicked: +localStorage.getItem("clicked"), clickCounter: +localStorage.getItem("clickCounter") });
+        }
 
-    //     if (this.state.clicked >= minMooseForUpgrade) {
-    //         this.setState({
-    //             clicked: this.state.clicked - minMooseForUpgrade,
-    //             clickCounter: this.state.clickCounter + incrClick
-    //         });
-    //         // localStorage.setItem('clickCounter', this.state.clickCounter, 'clicked', this.state.clicked - this.state.hornsMin);
-    //     }
-    // }
-
-    clicked1() {
-        console.log('1111');
+        window.addEventListener("beforeunload", (ev) => {
+            localStorage.setItem("clicked", this.state.clicked);
+            localStorage.setItem("clickCounter", this.state.clickCounter);
+        });
     }
+
+    upgradeClickHandler(minMooseForUpgrade, incrClick) {
+
+        if (this.state.clicked >= minMooseForUpgrade) {
+            this.setState({
+                clicked: this.state.clicked - minMooseForUpgrade,
+                clickCounter: this.state.clickCounter + incrClick
+            });
+        }
+    }
+
+    mooseClickHandler() {
+
+        this.setState({ clicked: this.state.clicked + this.state.clickCounter });
+    }
+
 
     render() {
         {
 
             return (
-                <div className="menu">
-                    {upgrades.map(function (num, index) {
-                        return (
-                            <div className="upgrade" key={index + 1} onClick={(e) => this.clicked1}>{num.name}({num.mooseMin})</div>
-                        )
-                    })
-                    }
+                <div>
+                    <div className="Moose">
+                        <img src={moose} onClick={this.mooseClickHandler} alt="moose" />
+                    </div>
+                    <div className="menu">
+                        {upgrades.map((num, index) => {
+                            return (
+                                <div className="upgrade" key={index + 1} onClick={this.upgradeClickHandler.bind(this, num.mooseMin, index + 1)}>{num.name}({num.mooseMin})</div>
+                            )
+                        })
+                        }
+                    </div>
+                    <div className="scoreboard">
+                        Лосиков: {this.state.clicked}
+                    </div>
                 </div>
+
             );
         }
 
