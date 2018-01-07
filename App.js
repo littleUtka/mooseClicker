20082481c1c4f1bdcde696e0eb90a66ca8fa3c6e
Denis Pevkin <qqwkaman@gmail.com>
2018-01-07 20:14:27 +0300

    Add files via upload
    Added buttons which can change upgrades

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css';
+import Upgrades from './Upgrades.js';
+import Achievements from './Achievements.js';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: 0,
+      clickCounter: 1,
+      dps: 0,
+      upgrades: [0, 0, 0, 0, 0],
+      business: [0, 0, 0, 0],
+      buttonUpgrade: [1, 10, 100]
+    }
+
+    this.mooseClickHandler = this.mooseClickHandler.bind(this);
+    this.upgradeClickHandler = this.upgradeClickHandler.bind(this);
+  }
+
+  componentWillMount() {
+
+    if (localStorage.getItem("params") !== null) {
+
+      let storageJSON = JSON.parse(localStorage.getItem("params"));
+      let newUpgrades = [storageJSON["hornsMin"], storageJSON["bloomMin"], storageJSON["sleighMin"], storageJSON["scooterMin"], storageJSON["rocketMin"]];
+
+      this.setState({ clicked: +storageJSON["clicked"], clickCounter: +storageJSON["clickCounter"], dps: +storageJSON["dps"], upgrades: newUpgrades });
+    } else {
+
+      this.setState({ clicked: 0, clickCounter: 1, dps: 0 });
+
+    }
+
+    setInterval(() => {
+      this.setState({ clicked: this.state.clicked + this.state.dps });
+    }, 1000);
+
+    window.addEventListener("beforeunload", (ev) => {
+
+      localStorage.setItem("params", '{"clicked":' + this.state.clicked + ',"clickCounter":' + this.state.clickCounter + ',"dps":' + this.state.dps +
+      ',"hornsMin":' + this.state.upgrades[0] + ',"bloomMin":' + this.state.upgrades[1] + ',"sleighMin":' + this.state.upgrades[2] + ',"scooterMin":' +
+      this.state.upgrades[3] + ',"rocketMin":' + this.state.upgrades[4] + '}')
+
+    });
+  }
+
+  mooseClickHandler() {
+
+    this.setState({ clicked: this.state.clicked + this.state.clickCounter });
+
+  }
+
+  upgradeClickHandler(incrClick, dpsUpgrade, minUpgrade, multiplier, flag) {
+   
+
+    if (this.state.clicked >= minUpgrade) {
+      let arrayUpgrage = [];
+      flag ? arrayUpgrage = this.state.upgrades : arrayUpgrage = this.state.business;
+      let oldClicked = arrayUpgrage[incrClick] * multiplier;
+      arrayUpgrage[incrClick] = (arrayUpgrage[incrClick] * Math.pow(1.25, multiplier)).toFixed(0);
+      
+      this.setState({
+        clicked: this.state.clicked - oldClicked,
+        clickCounter: this.state.clickCounter + (incrClick + 1) * multiplier,
+        dps: this.state.dps + +dpsUpgrade * multiplier,
+      });
+
+      flag ? this.setState({
+        upgrades: arrayUpgrage
+      }) : this.setState({
+        business: arrayUpgrage
+      }) 
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Upgrades handleMooseClick={this.mooseClickHandler} appState={this.state} handleUpgradeClick={this.upgradeClickHandler} />
+        <Achievements mooseClicked={this.state.clicked} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
---
+++ Upgrades.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import './App.css';
+import moose from './moose.png';
+var upgrades = require('./upgradesConfig.json');
+var business = require('./businessConfig.json');
+
+
+export default class Upgrades extends Component {
+    constructor() {
+        super();
+        this.state = {
+            upgradeFlag: true
+        }
+    }
+
+    returnMoose(mooseConv) {
+
+        if (mooseConv < Math.pow(10, 3)) {
+            return (mooseConv);
+        } else if ((mooseConv >= Math.pow(10, 3)) && (mooseConv < Math.pow(10, 6))) {
+            return ((mooseConv / Math.pow(10, 3)).toFixed(2) + 'K');
+        } else {
+            return ((mooseConv / Math.pow(10, 6)).toFixed(2) + 'М');
+        }
+
+    }
+
+    returnButtonColor(totalClicked, minMoose) {
+
+        let butonColor = totalClicked >= minMoose ? "#86C9CB" : "#A58686";
+        return (butonColor);
+
+    }
+
+    returnUpgradesMoose(upgradeKind, upgradeInfo, upgradeCount) {
+        
+        var dpsText = '';
+        return (upgradeKind.map((num, index) => {
+
+            num.name[2] !== "" ? dpsText = num.name[2] + " DPS" : dpsText = "";
+
+            if (upgradeCount === 1) {
+                if (this.props.appState.upgrades[index] === 0) {
+                    this.props.appState.upgrades[index] = num.name[1];
+                } // Проверка, что первоначальные данные взяты всего один раз
+            } else {
+                if (this.props.appState.business[index] === 0) {
+                    this.props.appState.business[index] = num.name[1];
+                } // Проверка, что первоначальные данные взяты всего один раз    
+            }
+            
+            let availableUpgrade = this.props.appState.clicked >= upgradeInfo[index];
+            // console.log(upgradeInfo[index]);
+            return (
+                <div className={availableUpgrade ? "upgradeLight" : "upgradeDark"} key={index + 1}>
+                    <div className="upgrade-text">
+                        {num.name[0]}<br />+{index + 1} per click<br />{dpsText}
+                    </div>
+                    <div className="button-upgrade">
+                        {this.props.appState.buttonUpgrade.map((buttonUpg, buttonIndex) => {
+                            return (
+
+                                <button className={"x" + buttonUpg} style={{ "backgroundColor": this.returnButtonColor(this.props.appState.clicked, upgradeInfo[index] * buttonUpg) }} onClick={(e) => this.props.handleUpgradeClick(index, num.name[2], upgradeInfo[index], buttonUpg, this.state.upgradeFlag)} key={buttonIndex}>
+                                    {"x" + buttonUpg}({this.returnMoose(upgradeInfo[index] * buttonUpg)})
+                                                        </button>
+                            )
+                        }
+                        )}
+                    </div>
+                    <progress value={(this.props.appState.clicked / upgradeInfo[index]) * 100} max="100" />
+                </div>
+            )
+        }))
+    }
+
+    handlerUpgradeStatus(statFlag) {
+        statFlag ? this.setState({ upgradeFlag: true }) : this.setState({ upgradeFlag: false });
+    }
+
+    render() {
+
+        return (
+            <div className="menu">
+                <div>
+                    <button className="button-upgrades" onClick={this.handlerUpgradeStatus.bind(this, false)}>Добыча</button>
+                    <button className="button-upgrades" onClick={this.handlerUpgradeStatus.bind(this, true)}>Бизнес</button>
+                </div>
+                <div className="Moose">
+                    <img src={moose} onClick={(e) => {
+
+                        this.props.handleMooseClick();
+
+                    }} alt="moose" />
+                </div>
+                {
+                    this.state.upgradeFlag ? this.returnUpgradesMoose(upgrades, this.props.appState.upgrades, 1) : this.returnUpgradesMoose(business, this.props.appState.business, 2)
+                }
+                <div className="scoreboard">
+                    Лосиков: {this.returnMoose(this.props.appState.clicked)}<br />PC: {this.returnMoose(this.props.appState.clickCounter)}<br />DPS: {this.returnMoose(this.props.appState.dps)}
+                </div>
+            </div>
+        );
+
+    }
+}
